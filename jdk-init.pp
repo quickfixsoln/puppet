29130b0bfee857c89b2f129b0746f1a180f28a94
quickfix <quickfixsoln@gmail.com>
2018-05-20 08:18:08 +0530

    Update jdk-init.pp

--- jdk-init.pp
+++ jdk-init.pp
@@ -3,14 +3,16 @@ class jdk8 ( $home_dir ){
  $jdk_file = "${home_dir}/jdk"  
 file{"/tmp/jdk-8u161-linux-x64.tar.gz":
       ensure => file,
-      source => "puppet:///extra_files/jdk-8u161-linux-x64.tar.gz"
+      source => "puppet:///extra_files/jdk-8u161-linux-x64.tar.gz",
+      beofore = Exec['extract the jdk tar file']
       }
 
   exec { 'extract the jdk tar file':
     command => 'tar -xf /tmp/jdk-8u161-linux-x64.tar.gz',
     cwd => '/home/vagrant',
     path    => '/usr/local/bin/:/bin/',
-    require => File["/tmp/jdk-8u161-linux-x64.tar.gz"]
+    require => File["/tmp/jdk-8u161-linux-x64.tar.gz"],
+    notify => File['$jdk_file']
        }
 
   file { $jdk_file:
